d1a0bfc58653d79ec1ec3ec8a725a50e8c82c9d0
Hsu <edittye379@gmail.com>
2016-06-20 16:19:52 +0800

    基本登入註冊

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::auth();
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index');
