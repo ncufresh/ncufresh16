08f46283de8c1d2ac7f1d9d558f484f81a8bfb23
Hsu <edittye379@gmail.com>
2016-07-05 00:16:41 +0800

    整理路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,17 +9,24 @@ Route::auth();
 Route::get('/', 'HomeController@index');
 //************************************************************
 
-
+// 校園導覽
+//************************************************************
 Route::get('/campus','CampusController@index');
 Route::get('/campus/guide','CampusController@guide');
 Route::get('/campus/newData','CampusController@newData');
 Route::get('/campus/create','CampusController@createData');
+//************************************************************
 
-
+// 系所社團
+//************************************************************
 Route::resource('group', 'GroupController');
 
 Route::get('groups', function () {
     return view('groups.index');
 });
+//************************************************************
 
+// 小遊戲
+//************************************************************
 Route::get('/smallgame', 'HomeController@index');
+//************************************************************
