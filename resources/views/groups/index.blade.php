0c5165c14984ace5e954e323305a9097df291931
Hsu <edittye379@gmail.com>
2016-07-06 00:33:43 +0800

    標題改回來

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', '系所社團')
 @section('content')
 	<div>
 		<a href="{{ url('/groups/departments') }}">
@@ -6,15 +7,13 @@
 			<span>系所</span>
 		</a>
 
-		
+
 	</div>
 	<div>
 		<a href="{{ URL::action('ClubController@index') }}">
 			<img src="{{ asset('image/club.jpg')  }}">
 			<span>社團</span>
 		</a>
-		
+
 	</div>
 @endsection
-
-
