c0d9c3539fb14f45ddeee66beb79797dc9dca475
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-07-05 22:20:38 +0800

    i can fly

---
+++ app/Question_collection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question_collection extends Model
+{
+    protected $table = 'question_collection';//name of table
+    
+}
