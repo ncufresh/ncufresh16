dff4e644ae890f694ca3ddfee3ca1c61f4c47d87
Bear <william456821@g.ncu.edu.tw>
2016-07-05 20:45:31 +0800

    Q&A 簡易版本

---
+++ app/QandA.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QandA extends Model
+{
+    protected $fillable = ['content','classify'];
+    protected $table = "qanda";
+}
---
+++ database/migrations/2016_07_05_113203_create_QandA_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQandATable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('QandA', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('classify');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('QandA');
+    }
+}
