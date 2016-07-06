5a187373147914224b7e0d68fef479aaa28f582a
Hsu <edittye379@gmail.com>
2016-07-06 23:27:02 +0800

    重複的app.php

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -207,8 +206,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
 
         // 套件件
         'Shinobi' => Caffeinated\Shinobi\Facades\Shinobi::class,
