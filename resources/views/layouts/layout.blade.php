e6a3bb791f2bb730126bad73dc15b459ba111f8b
Hsu <edittye379@gmail.com>
2016-07-12 09:31:23 +0800

    將material fonts存在本機

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,23 +2,25 @@
 <html lang="zh-Hant">
 <head>
     <title>@yield('title', '記得改標題!!不是改我')</title>
-    <!-- mete區 -->
+    {{-- mete區 --}}
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <!-- 引入Styles -->
+    {{-- 引入Styles --}}
     <link rel="stylesheet" href="{{ asset('include/bootstrap/css/bootstrap.css') }}" media="screen">
     <link rel="stylesheet" href="{{ asset('include/font-awesome/css/font-awesome.css') }}" media="screen">
-    <!-- Material Design fonts -->
-    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/icon?family=Material+Icons">
-    <!-- Bootstrap Material Design -->
+    {{-- Material Design fonts --}}
+    {{--<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700">--}}
+    {{--<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/icon?family=Material+Icons">--}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('include/bootstrap-material/fonts/family-roboto.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('include/bootstrap-material/fonts/family-material-icons.css') }}">
+    {{-- Bootstrap Material Design --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('include/bootstrap-material/css/bootstrap-material-design.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('include/bootstrap-material/css/ripples.css') }}">
 
-    <!-- 個人Styles -->
+    {{-- 個人Styles --}}
     <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
     @yield('css')
 
@@ -31,7 +33,7 @@
 
     @include('layouts.footer')
 
-    <!-- JavaScripts -->
+    {{-- JavaScripts --}}
     <script src="{{ asset('include/jquery/jquery-1.12.4.js') }}"></script>
     <script src="{{ asset('include/jquery/jquery.ujs.js') }}"></script>
     <script src="{{ asset('include/bootstrap/js/bootstrap.js') }}"></script>
