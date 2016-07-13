ff95cee803c1a0d57ca391a3c7f259b08d26b163
Hsu <edittye379@gmail.com>
2016-07-13 19:29:03 +0800

    修正migrate class名稱

--- database/migrations/2016_07_11_044353_create_lives_table.php
+++ database/migrations/2016_07_11_044353_create_lives_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLifesTable extends Migration
+class CreateLivesTable extends Migration
 {
     /**
      * Run the migrations.
