e9c2a37f859c3717d1758d0dc3ff6fd9017e976a
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-07-15 15:19:03 +0800

    add the add_question Backstage

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,6 +11,7 @@ use App\Record_score;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
+
 class GameController extends Controller
 {
     public function index(){
@@ -38,4 +39,49 @@ class GameController extends Controller
         $scores=Question_collection::all();
         return response()->json($question); //為何跟Rounter的寫法需要不一樣?
     }
+
+
+    public function addQuestion(){//開啟新增問題頁面
+        $questions=Question_collection::all();
+
+        return view('smallgame.add_question')->with('questions',$questions);;
+    }
+
+    public function add(Request $request){
+        $encrypter = app('Illuminate\Encryption\Encrypter');
+        $encrypted_token = $encrypter->encrypt(csrf_token());
+        $scores = Question_collection::create([
+            'question'=>$request->question,
+            'selection_1'=>$request->selection_1,
+            'selection_2'=>$request->selection_2,
+            'answer'=>$request->answer
+            ]);
+        return response()->json($scores);
+
+    }
+
+    public function getOneQuestion($question_id){
+        $aQuestion=Question_collection::find($question_id);
+
+        return response()->json($aQuestion);
+    }
+
+    public function putOneQuestion(Request $request,$question_id){
+        $questions = Question_collection::find($question_id);//create a new model instance 
+
+        $questions->question = $request->question;
+        $questions->selection_1 = $request->selection_1;
+        $questions->selection_2 = $request->selection_2;
+        $questions->answer = $request->answer;
+        
+
+        $questions->save();//call the save method  , the method can input data into the database
+
+        return response()->json($questions);
+    }
+    public function deleteOneQuestion($question_id){
+        $question = Question_collection::destroy($question_id);
+        return response()->json($question);
+    }   
+
 }
