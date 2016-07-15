754b73d3d192575a2b7d2df9d45108787c6d39f9
JoNz94 <jonz94@g.ncu.edu.tw>
2016-07-15 21:03:45 +0800

    套用 layout

--- resources/views/documents/graduate.blade.php
+++ resources/views/documents/graduate.blade.php
@@ -1,39 +1,59 @@
-{{-- 顯示研究所的新生必讀資料 --}}
-<div>
-    <h1>研究所　<small><a href="{{ url('/doc/under') }}">大學部</a></small></h1>
-    <ul>
-        <?php $count = 0; ?>
-        @foreach ($mainGraduates as $graduates)
-        <li>研究所主條目 {{ ++$count }}</li>
+@extends('layouts.layout')
+
+@section('title', '新生必讀 - 研究所')
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        {{-- 顯示研究所的新生必讀資料 --}}
+        <h1>研究所　<small><a href="{{ url('/doc/under') }}">大學部</a></small></h1>
         <ul>
-            @foreach ($graduates as $g)
-                <li>{{ $g->title }}</li>
+            <?php $count = 0; ?>
+            @foreach ($mainGraduates as $graduates)
+                <li>研究所主條目 {{ ++$count }}</li>
                 <ul>
-                    <li>{{ $g->content }}</li>
-                    <form action="{{ url('/doc/graduate/'.$g->id.'/edit') }}" method="GET">
-                        <button type="submit" id="edit-document-{{ $g->id }}">編輯</button>
-                    </form>
-                    <form action="{{ url('/doc/graduate/'.$g->id) }}" method="POST">
-                        {!! csrf_field() !!}
-                        {!! method_field('DELETE') !!}
-                        <button type="submit" id="delete-document-{{ $g->id }}">刪除</button>
-                    </form>
+                    @foreach ($graduates as $g)
+                        <li>{{ $g->title }}</li>
+                        <ul>
+                            <li>{{ $g->content }}</li>
+                            <form action="{{ url('/doc/graduate/'.$g->id.'/edit') }}" method="GET">
+                                <button type="submit" id="edit-document-{{ $g->id }}">編輯</button>
+                            </form>
+                            <form action="{{ url('/doc/graduate/'.$g->id) }}" method="POST">
+                                {!! csrf_field() !!}
+                                {!! method_field('DELETE') !!}
+                                <button type="submit" id="delete-document-{{ $g->id }}">刪除</button>
+                            </form>
+                        </ul>
+                    @endforeach 
                 </ul>
-            @endforeach 
+            @endforeach
         </ul>
-        @endforeach
-    </ul>
+    </div>
+    {{-- 新增研究所的新生必讀資料 --}}
+    <div class="row">
+        <form action="{{ url('/doc/graduate') }}" method="POST">
+            {{ csrf_field() }}
+            <p>標題</p>
+            <p><input type="textbox" name="title"></p>
+            <p>內文</p>
+            <p><input type="textbox" name="content"></p>
+            <p>隸屬於哪個主項目</p>
+            <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
+            <p><button type="submit">新增</button></p>
+        </form>
+    </div>
 </div>
-{{-- 新增研究所的新生必讀資料 --}}
-<div>
-    <form action="{{ url('/doc/graduate') }}" method="POST">
-        {{ csrf_field() }}
-        <p>標題</p>
-        <p><input type="textbox" name="title"></p>
-        <p>內文</p>
-        <p><input type="textbox" name="content"></p>
-        <p>隸屬於哪個主項目</p>
-        <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
-        <p><button type="submit">新增</button></p>
-    </form>
-</div>
+
+@endsection
+
+
--- resources/views/documents/under.blade.php
+++ resources/views/documents/under.blade.php
@@ -1,48 +1,57 @@
-{{-- 顯示大學部的新生必讀資料
+@extends('layouts.layout')
 
-unders 為 Array 分別代表各個主項目內的子項目
+@section('title', '新生必讀 - 大學部')
 
-ex:
-unders[0]          為第一主項目的各個子項目
-unders[0]->title   為該子項目的標題
-unders[0]->content 為該子項目的內文
+@section('css')
 
---}}
-<div>
-    <h1>大學部　<small><a href="{{ url('/doc/graduate') }}">研究所</a></small></h1>
-    <ul>
-        <?php $count = 0; ?>
-        @foreach ($mainUnders as $unders)
-            <li>大學部主條目 {{ ++$count }}</li>
-            <ul>
-                @foreach ($unders as $u)
-                <li>{{ $u->title }}</li>
+@stop
+
+@section('js')
+
+@stop
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        {{-- 顯示大學部的新生必讀資料 --}}
+        <h1>大學部　<small><a href="{{ url('/doc/graduate') }}">研究所</a></small></h1>
+        <ul>
+            <?php $count = 0; ?>
+            @foreach ($mainUnders as $unders)
+                <li>大學部主條目 {{ ++$count }}</li>
                 <ul>
-                    <li>{{ $u->content }}</li>
-                    <form action="{{ url('/doc/under/'.$u->id.'/edit') }}" method="GET">
-                        <button type="submit" id="edit-under-{{ $u->id }}">編輯</button>
-                    </form>
-                    <form action="{{ url('/doc/under/'.$u->id) }}" method="POST">
-                        {!! csrf_field() !!}
-                        {!! method_field('DELETE') !!}
-                    <button type="submit" id="delete-document-{{ $u->id }}">刪除</button>
-                </form>
+                    @foreach ($unders as $u)
+                        <li>{{ $u->title }}</li>
+                        <ul>
+                            <li>{{ $u->content }}</li>
+                            <form action="{{ url('/doc/under/'.$u->id.'/edit') }}" method="GET">
+                                <button type="submit" id="edit-under-{{ $u->id }}">編輯</button>
+                            </form>
+                            <form action="{{ url('/doc/under/'.$u->id) }}" method="POST">
+                                {!! csrf_field() !!}
+                                {!! method_field('DELETE') !!}
+                            <button type="submit" id="delete-document-{{ $u->id }}">刪除</button>
+                        </form>
+                        </ul>
+                    @endforeach 
                 </ul>
-                @endforeach 
-            </ul>
-        @endforeach
-    </ul>
+            @endforeach
+        </ul>
+    </div>
+    {{-- 新增大學部的新生必讀資料 --}}
+    <div class="row">
+        <form action="{{ url('/doc/under') }}" method="POST">
+            {{ csrf_field() }}
+            <p>標題</p>
+            <p><input type="text" name="title" id="title" required></p>
+            <p>內文</p>
+            <p><input type="text" name="content" id="content" required></p>
+            <p>隸屬於哪個主項目</p>
+            <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
+            <p><button type="submit">新增</button></p>
+        </form>
+    </div>
 </div>
-{{-- 新增大學部的新生必讀資料 --}}
-<div>
-    <form action="{{ url('/doc/under') }}" method="POST">
-        {{ csrf_field() }}
-        <p>標題</p>
-        <p><input type="text" name="title" id="title" required></p>
-        <p>內文</p>
-        <p><input type="text" name="content" id="content" required></p>
-        <p>隸屬於哪個主項目</p>
-        <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
-        <p><button type="submit">新增</button></p>
-    </form>
-</div>
+
+@endsection
