c45320ebdc2a0fc5f57f92f68d3b1e33aeb73832
Hsu <edittye379@gmail.com>
2016-07-22 03:24:22 +0800

    新footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,54 @@
-<footer id="footer">
-      <div class="container text-center">
-        <a class="up-arrow" href="#app-layout" data-toggle="tooltip" title="To top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </a><br><br>
-        <p>國立中央大學2016新生知訊網團隊 版權所有 © 2016 NCU Fresh All Rights Reserved</p>
-        <p>建議使用Chrome、Firefox或者IE11等瀏覽器，以獲得最佳體驗</p>
-      </div>
+{{-- Footer --}}
+<footer>
+{{-- 上footer --}}
+<div class="footer-above">
+<div class="container">
+<div class="row">
+    <div class="footer-col col-md-4">
+        <h3>常用連結</h3>
+        <div class="row">
+            <div class="col-xs-4">
+                <ul class="footer-ul">
+                    <li><a href="#">中大首頁</a></li>
+                    <li><a href="#">中大Portal</a></li>
+                    <li><a href="#">LMS</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-8">
+                <ul class="footer-ul">
+                    <li><a href="#">中大首頁</a></li>
+                    <li><a href="#">中大Portal</a></li>
+                    <li><a href="#">LMS</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-col col-md-4">
+        <h3>社群網站</h3>
+        <ul class="list-inline">
+            <li><a href="https://www.facebook.com/groups/NCUgroup/" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a></li>
+            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCmO3cFtFtdIawfBQpGR8EVA" class="btn-social btn-outline"><i class="fa fa-fw fa-youtube"></i></a></li>
+            <li><a href="http://www.ncu.edu.tw/" class="btn-social btn-outline"><i class="fa fa-mortar-board"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-col col-md-4">
+        <h3>關於我們</h3>
+        <p><i class="fa fa-info-circle"></i> &nbsp;我們是由一群大一~大三生組成的團隊</p>
+        <p><i class="fa fa-external-link"></i> <a href="{{ url('/') }}">想知道更多關於新生知訊網團隊的成員嗎?</a></p>
+        <p><i class="fa fa-envelope-o"></i>&nbsp; E-mail : ncufresh@gmail.com</p>
+    </div>
+</div>
+</div>
+</div>
+{{-- 下footer --}}
+<div class="footer-below text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                國立中央大學2016新生知訊網團隊 版權所有 &copy; 2016 NCU Fresh All Rights Reserved
+            </div>
+        </div>
+    </div>
+</div>
 </footer>
