b6b0c28c798d6387b26bb974eb7d8812a23059d8
Hsu <edittye379@gmail.com>
2016-07-23 03:14:07 +0800

    新增公告內容的預覽欄位

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Announcement;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $anns = Announcement::orderBy('is_top','desc')->orderBy('post_at','desc')->get();
+
+        // 預覽公告內容欄位
+        $anns = $anns->each(function ($item, $key) {
+            $tmp = strip_tags($item->content); // html tag全移除
+            $item->short_content=substr($tmp, 0, 10); // 只要前五個字
+        });
+
+        return view('home', [
+            'anns' => $anns,
+        ]);
     }
 }
