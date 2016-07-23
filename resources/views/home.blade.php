11f71b6239d4a940f3ce4f905793c275f42c854b
Hsu <edittye379@gmail.com>
2016-07-23 19:48:11 +0800

    首頁隨機變色

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,19 @@
 @section('title', 'NCU Fresh | 新生知訊網')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('include/izimodal/css/iziModal.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 @stop
 
 @section('js')
 <script src="{{ asset('include/jquery/jquery.backstretch.min.js') }}"></script>
+<script src="{{ asset('include/izimodal/js/iziModal.min.js') }}"></script>
 <script src="{{ asset('js/home.js') }}"></script>
 @stop
 
 @section('content')
 {{-- 到最上面的按鈕 --}}
 <a href="#app-layout" class="btn btn-fab" id="totop"><i class="material-icons">arrow_upward</i></a>
-
-{{-- div#all 放全部內容還有四季圖 --}}
-<div id="all">
 
 {{-- 第一區 logo --}}
 <header id="logo">
@@ -25,7 +24,7 @@
             <div class="intro-text">
                 <img class="img-responsive center-block" src="{{ asset('img/home/logo.png') }}">
                 <h1 class="text-center">新生都該擁有的校園生活攻略</h1>
-                <br>
+                {{-- <br> --}}
                 <a href="#news" class="btn btn-circle">
                     <i class="fa fa-angle-double-down fa-3x"></i>
                 </a>
@@ -39,7 +38,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12 text-center">
-            <h2 class="section-heading">最新消息</h2>
+            <h1 class="section-heading">最新消息</h1>
             <h4 class="section-subheading text-muted">我們將會定期更新最新消息</h4>
         </div>
     </div>
@@ -47,32 +46,47 @@
     <div class="row">
         <div class="col-md-6" id="ann">
             <div class="list-group">
-              <a href="#">
-                  <div class="list-group-item">
-                      <div class="row-action-primary">
-                          <i class="material-icons">folder</i>
-                      </div>
-                      <div class="row-content">
-                          <div class="least-content">2016 / 8 / 8</div>
-                          <h4 class="list-group-item-heading">標題標題標題</h4>
-                          <p class="list-group-item-text">內容內容內容內容內容.....</p>
-                      </div>
-                  </div>
-              </a>
-              <div class="list-group-separator"></div>
-              <a href="#">
-                  <div class="list-group-item">
-                      <div class="row-action-primary">
-                          <i class="material-icons">folder</i>
-                      </div>
-                      <div class="row-content">
-                          <div class="least-content">2016 / 8 / 8</div>
-                          <h4 class="list-group-item-heading">標題標題標題</h4>
-                          <p class="list-group-item-text">內容內容內容內容內容.....</p>
-                      </div>
-                  </div>
-              </a>
-              <div class="list-group-separator"></div>
+                @foreach ($anns as $ann)
+                <a href="#" data-toggle="modal" data-target="#myModal{{ $ann->id }}">
+                    <div class="list-group-item">
+                        <div class="row-action-primary">
+                        @if($ann->is_top)
+                            <i class="material-icons">face</i>
+                        @else
+                            <i class="material-icons">folder</i>
+                        @endif
+                        </div>
+                        <div class="row-content">
+                            <div class="least-content">{{ $ann->post_at }}</div>
+                            <h4 class="list-group-item-heading">{{ $ann->title }}</h4>
+                            <p class="list-group-item-text">{{ $ann->short_content }} ..........</p>
+                        </div>
+                    </div>
+                </a>
+                <div class="list-group-separator"></div>
+
+                <!-- Modal -->
+                <div id="myModal{{ $ann->id }}" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+                      <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h2 class="modal-title">標題: {{ $ann->title }}</h2>
+                                <h4 class="modal-title">發佈日期: {{ $ann->post_at }}</h4>
+                            </div>
+                            <div class="modal-body">
+                                {!! $ann->content !!}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">
+                                    <span class="glyphicon glyphicon-remove"></span> Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
         <div class="col-md-6">
@@ -127,92 +141,98 @@
 </div>
 <div id="timeline">
 	  <div class="timeline-item">
-  			<div class="timeline-icon">
-            <i class="material-icons">rss_feed</i>
-  			</div>
-				<div class="timeline-content">
+  			<a href="#">
+            <div class="timeline-icon">
+                <i class="material-icons">rss_feed</i>
+  			    </div>
+        </a>
+				<div class="timeline-content left">
   					<h2>新生必讀</h2>
   					<p>
               QQ
             </p>
-  					<a href="#" class="btn">現在就去</a>
 				</div>
 		</div>
 		<div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">cloud</i>
-				</div>
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">cloud</i>
+    				</div>
+        </a>
   			<div class="timeline-content right">
   					<h2>新生Q&amp;A</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
 		<div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">directions_bike</i>
-				</div>
-  			<div class="timeline-content">
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">directions_bike</i>
+    				</div>
+        </a>
+  			<div class="timeline-content left">
       			<h2>校園導覽</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
     <div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">face</i>
-				</div>
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">face</i>
+    				</div>
+        </a>
   			<div class="timeline-content right">
   					<h2>系所社團</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
     <div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">pets</i>
-				</div>
-  			<div class="timeline-content">
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">pets</i>
+    				</div>
+        </a>
+  			<div class="timeline-content left">
       			<h2>中大生活</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
     <div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">play_circle_outline</i>
-				</div>
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">play_circle_outline</i>
+    				</div>
+        </a>
   			<div class="timeline-content right">
   					<h2>影音專區</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
     <div class="timeline-item">
-				<div class="timeline-icon">
-            <i class="material-icons">bubble_chart</i>
-				</div>
-  			<div class="timeline-content">
+        <a href="#">
+    				<div class="timeline-icon">
+                <i class="material-icons">bubble_chart</i>
+    				</div>
+        </a>
+  			<div class="timeline-content left">
   					<h2>關於我們</h2>
             <p>
               QQ
             </p>
-            <a href="#" class="btn">現在就去</a>
   			</div>
 		</div>
 </div>
 </div>
 </section>
 
-</div> {{-- div#all --}}
 @endsection
