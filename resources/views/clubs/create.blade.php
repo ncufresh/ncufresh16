19bc2e6369c91dbc6ad732cd41439e0b61cdeffa
Michelle <qvqv621@gmail.com>
2016-07-23 20:10:56 +0800

    幻燈片好了還有編輯

--- resources/views/clubs/create.blade.php
+++ resources/views/clubs/create.blade.php
@@ -1,17 +1,10 @@
 @extends('layouts.layout')
 
-@section('js')
-<script src="{{ asset('vendor/laravel-filemanager/js/lfm.js') }}"></script>
-<script type="text/javascript">
-$('#lfm').filemanager('image');
 
-</script>
-@stop
 
 @section('content')
-@foreach($test as $t)
-<p>{{$t}}</p>
-@endforeach
+
+
 	<form action="{{ URL::action('ClubController@store') }}" method="post" enctype="multipart/form-data">
 	{{csrf_field()}}
 		<br><br><br><br><br><br><br><br>
@@ -32,9 +25,8 @@ $('#lfm').filemanager('image');
 			<input type="text" name="clubs_intro">
 			<label>社團名稱</label>
 		</div>
-
-
-		
+ 		
+		<!-- 社團封面照 -->		
 		<div class="input-group">
 			<span class="input-group-btn">
 		    <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
@@ -46,6 +38,9 @@ $('#lfm').filemanager('image');
        		</input>
 		</div>
 		<img id="holder" style="margin-top:15px;max-height:100px;">
+
+
+
 
 		<div>
 			<input type="text" name="clubs_summary">
@@ -62,23 +57,42 @@ $('#lfm').filemanager('image');
 			<label>如何參加</label>
 		</div>
 
+		<input id="button" type="button" value="Add photo" />
+ 		<div id="test"></div>
+			
+		
 		<th colspan="2">
             <input type="hidden" name="_token" value="{{csrf_token()}}"/>
             <input type="submit" value="submit">
         </th>
-        
-		<div class="input-group">
-			<span class="input-group-btn">
-		    <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-		      <i class="fa fa-picture-o"></i> Choose
-		    </a>
-		  	</span>
-		  	<input id="thumbnail" class="form-control" type="text" name="clubs_file">
-		  	
-       		</input>
-		</div>
-			
+	
 	</form>
 
+	@section('js')
+	<script src="{{ asset('vendor/laravel-filemanager/js/lfm.js') }}"></script>
+	<script type="text/javascript">
+	$('#lfm').filemanager('image');
+	
+	$(document).ready(function(){
+		var r = 1;
+		$('#button').click(function(){
+			$('#test').append('<div class="input-group"><span class="input-group-btn"><a id="lfm'+r+ 
+				'" data-input="thumbnail'+r+
+				'" data-preview="holder'+r+
+				'" class="btn btn-primary"><i class="fa fa-picture-o"></i> Choose</a></span><input id="thumbnail'+r+
+				'" class="form-control" type="text" name="clubs_photo[]"></input></div><img id="holder'+r+
+				'" style="margin-top:15px;max-height:100px;">');
+					$('#lfm' + r ).filemanager('image');
+			r++;
+
+		});
+
+	});
+
+	</script>
+	@stop
 	
 @endsection
+
+
+
--- resources/views/clubs/show.blade.php
+++ resources/views/clubs/show.blade.php
@@ -3,19 +3,20 @@
 	<br><br><br><br>	
 	<a href="{{ url('/groups/clubs/create') }}">新增</a>
 	@foreach ($clubs as $club)
-		
+		<?php $photo = json_decode($club->clubs_photo); ?>
 		
 		<div class="container">
 		  
 		  <!-- Trigger the modal with a button -->
+		  <!-- 要{{$club->id}} 才不會只顯示第一筆資料 -->
 		  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal{{$club->id}}">{{$club->clubs_intro}}<img src="{{$club->clubs_file}}"></button>
 
 		  <!-- Modal -->
-		  <!-- {{$club->id}} 不會只顯示第一筆資料 -->
+		  <!-- 要{{$club->id}} 才不會只顯示第一筆資料 -->
 		  <div class="modal fade" id="myModal{{$club->id}}" role="dialog">
 		    <div class="modal-dialog">
 		      <!-- Modal content-->
-		      <div class="modal-content">
+		    <div class="modal-content">
 		        <div class="modal-header">
 		          <button type="button" class="close" data-dismiss="modal">&times;</button>
 		          <h4 class="modal-title">{{$club->clubs_intro}}</h4>
@@ -25,47 +26,54 @@
 		          <p>{{$club->clubs_activity}}</p>
 		          <p>{{$club->clubs_join}}</p>
 		        </div>
-
-		        <div class="container">
-				  <br>
-				  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-				    <!-- Indicators -->
-				    <ol class="carousel-indicators">
-				      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				      <li data-target="#myCarousel" data-slide-to="1"></li>
-				      
-				    </ol>
-
-				    <!-- Wrapper for slides -->
-				    <div class="carousel-inner" role="listbox">
-				      <div class="item active">
-				        <img src="images.jpg" alt="Chania" width="460" height="345">
-				      </div>
-
-				      <div class="item">
-				        <img src="picture.jpg" alt="Chania" width="460" height="345">
-				      </div>
+		        
+		        <div id="myCarousel{{$club->id}}" class="carousel slide" data-ride="carousel">
 				    
-				    </div>
-
-				    <!-- Left and right controls -->
-				    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-				      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				      <span class="sr-only">Previous</span>
-				    </a>
-				    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-				      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				      <span class="sr-only">Next</span>
-				    </a>
-				  </div>
+					    <!-- Indicators -->
+					    <ol class="carousel-indicators">
+					    @foreach ($photo as $key => $p)
+						    @if($key == 0)
+								<li data-target="#myCarousel" data-slide-to="{{$key}}" class="active"></li>
+							@else
+								<li data-target="#myCarousel" data-slide-to="{{$key}}"></li>
+							@endif
+						@endforeach
+						</ol>
+					 
+					    <!-- Wrapper for slides -->
+					    <div class="carousel-inner" role="listbox">
+					   	@foreach ($photo as $key => $p)
+						    @if($key == 0)
+								<div class="item active">
+						        	<img src="{{$p}}" alt="FailQQ">
+						      	</div>
+							@else
+								<div class="item">
+						        	<img src="{{$p}}" alt="FailQQ">
+						      	</div>
+						    @endif
+						@endforeach
+						</div>
+				    
+				     <!-- Left and right controls -->
+					<a class="left carousel-control" href="#myCarousel{{$club->id}}" role="button" data-slide="prev">
+					   <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					   <span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel{{$club->id}}" role="button" data-slide="next">
+					    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
+			</div>
 		        
 		        <div class="modal-footer">
 		          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 		        </div>
+
 		      </div>
 		    </div>
-		  </div>
+		</div>
 
 		  <!-- 幻燈片 -->
 		  
