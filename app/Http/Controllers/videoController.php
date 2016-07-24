67f533befe86ad4808a3601ff4f5eb54f688cdd6
Rose <i2sj0327@gmail.com>
2016-07-24 21:39:50 +0800

    我從台中回來了

--- app/Http/Controllers/videoController.php
+++ app/Http/Controllers/videoController.php
@@ -17,4 +17,59 @@ class videoController extends Controller
     		'videos' => $videos,
     		]);
     }
+
+	public function create()
+    {
+       return view('videos.create');
+    }
+
+	public function store(Request $request)
+	{
+    	$videos = new videos;
+    	$videos->videos_kind = $request->videos_kind;
+    	$videos->videos_intro = $request->videos_intro;
+    	$videos->videos = $request->videos;
+    	$videos->save();
+    	return redirect('/index');
+	}
+
+        public function live()
+    {
+    	$videos = videos::all();
+    	return view('videos.live', [
+    		'videos' => $videos,
+    		]);
+    }
+
+        public function traffic()
+    {
+    	$videos = videos::all();
+    	return view('videos.traffic', [
+    		'videos' => $videos,
+    		]);
+    }
+
+        public function edu()
+    {
+    	$videos = videos::all();
+    	return view('videos.edu', [
+    		'videos' => $videos,
+    		]);
+    }
+
+        public function fun()
+    {
+    	$videos = videos::all();
+    	return view('videos.fun', [
+    		'videos' => $videos,
+    		]);
+    }
+
+            public function ncu()
+    {
+    	$videos = videos::all();
+    	return view('videos.ncu', [
+    		'videos' => $videos,
+    		]);
+    }
 }
