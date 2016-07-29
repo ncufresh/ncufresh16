ed18b8ad5ea9c31dd6d2d5809bef9c225696ad36
JoNz94 <jonz94@g.ncu.edu.tw>
2016-07-29 15:54:01 +0800

    change modal layout

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -46,9 +46,9 @@ CKEDITOR.replace( 'new_mix', {
     <!-- 上方兩個按鈕畫面 -->
     <div class="jumbotron" id="topScreen">
         <div class="container-fluid">
-            <div class="row text-center little-button-container">
+            <div class="row little-button-container">
                 <!-- 左邊大學部導覽列 -->
-                <div class="col-xs-6" id="outerLeftSidebar">
+                <div class="col-xs-6 text-right" id="outerLeftSidebar">
                     <p><a href="#under-1"><img src="{{ asset('docs/img/col.png') }}" alt="kirby" id="openLeft"></a></p>
                     <p class="test1"></p>
                     <p class="test2"></p>
@@ -57,7 +57,7 @@ CKEDITOR.replace( 'new_mix', {
                 </div>
                 <!-- /左邊大學部導覽列 -->
                 <!-- 右邊研究所導覽列 -->
-                <div class="col-xs-6" id="outerRightSidebar">
+                <div class="col-xs-6 text-left" id="outerRightSidebar">
                     <p>
                         <a href="#graduate-1"><img src="{{ asset('docs/img/gra.png') }}" alt="kirby" id="openRight"></a>
                     </p>
@@ -102,16 +102,16 @@ CKEDITOR.replace( 'new_mix', {
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">大學部 - 新增內容</h4>
+                                <h1 class="modal-title">大學部 - 新增內容</h1>
                             </div>
                             <div class="modal-body">
                                 <form action="{{ url('/doc/under') }}" method="POST">
                                     {{ csrf_field() }}
-                                    <p>標題</p>
-                                    <p><input type="textbox" name="title" required></p>
-                                    <p>內文</p>
-                                    <p><textarea name="content" id="new_under" required></textarea></p>
-                                    <p>隸屬於哪個主項目</p>
+                                    <h3>標題</h3>
+                                     <p><input name="title" type="text" class="form-control" placeholder="Title" required></p>
+                                    <h3>內文</h3>
+                                    <textarea name="content" id="new_under" required></textarea>
+                                    <h3>隸屬於哪個主項目</h3>
                                     <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
                                     <p>
                                         <button type="submit" class="btn btn-primary">新增</button>
@@ -143,16 +143,32 @@ CKEDITOR.replace( 'new_mix', {
                                     <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal-{{ $u->id }}">Open Modal {{ $u->id }}</button>
                                 </p>
                                 <!-- Modal -->
-                                <div id="modal-{{ $u->id }}" class="modal fade" role="dialog">
+                                <div id="modal-{{ $u->id }}" class="modal fade text-left" role="dialog">
                                     <div class="modal-dialog modal-lg">
                                         <!-- Modal content-->
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">{{ $u->title }}</h4>
+                                                <h1 class="modal-title">{{ $u->title }}</h1>
                                             </div>
                                             <div class="modal-body">
-                                                <p>{!! $u->content !!}</p>
+                                                <div class="row">
+                                                    <p>{!! $u->content !!}</p>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-xs-6 text-right">
+                                                        <form action="{{ url('/doc/under/'.$u->id.'/edit') }}" method="GET">
+                                                            <button type="submit" class="btn btn-success btn-lg" id="edit-under-{{ $u->id }}">編輯</button>
+                                                        </form>
+                                                    </div>
+                                                    <div class="col-xs-6 text-left">
+                                                        <form action="{{ url('/doc/under/'.$u->id) }}" method="POST">
+                                                            {!! csrf_field() !!}
+                                                            {!! method_field('DELETE') !!}
+                                                            <button type="submit" class="btn btn-danger btn-lg" id="delete-document-{{ $u->id }}">刪除</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-default" data-dismiss="modal">關閉</button>
@@ -161,14 +177,7 @@ CKEDITOR.replace( 'new_mix', {
                                     </div>
                                 </div>
                                 <!-- /Modal -->
-                                <form action="{{ url('/doc/under/'.$u->id.'/edit') }}" method="GET">
-                                    <button type="submit" class="btn btn-success btn-lg" id="edit-under-{{ $u->id }}">編輯</button>
-                                </form>
-                                <form action="{{ url('/doc/under/'.$u->id) }}" method="POST">
-                                    {!! csrf_field() !!}
-                                    {!! method_field('DELETE') !!}
-                                    <button type="submit" class="btn btn-danger btn-lg" id="delete-document-{{ $u->id }}">刪除</button>
-                                </form>
+                                
                             </div>
                         @endforeach
                         </div>
@@ -199,16 +208,32 @@ CKEDITOR.replace( 'new_mix', {
                                     <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal-{{ $g->id }}">Open Modal {{ $g->id }}</button>
                                 </p>
                                 <!-- Modal -->
-                                <div id="modal-{{ $g->id }}" class="modal fade" role="dialog">
+                                <div id="modal-{{ $g->id }}" class="modal fade  text-left" role="dialog">
                                     <div class="modal-dialog modal-lg">
                                         <!-- Modal content-->
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">{{ $g->title }}</h4>
+                                                <h1 class="modal-title">{{ $g->title }}</h1>
                                             </div>
                                             <div class="modal-body">
-                                                <p>{!! $g->content !!}</p>
+                                                <div class="row">
+                                                    <p>{!! $g->content !!}</p>  
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-xs-6 text-right">
+                                                        <form action="{{ url('/doc/graduate/'.$g->id.'/edit') }}" method="GET">
+                                                            <button type="submit" class="btn btn-success btn-lg" id="edit-graduate-{{ $g->id }}">編輯</button>
+                                                        </form>
+                                                    </div>
+                                                    <div class="col-xs-6 text-left">
+                                                        <form action="{{ url('/doc/graduate/'.$g->id) }}" method="POST">
+                                                            {!! csrf_field() !!}
+                                                            {!! method_field('DELETE') !!}
+                                                            <button type="submit" class="btn btn-danger btn-lg" id="delete-document-{{ $g->id }}">刪除</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-default" data-dismiss="modal">關閉</button>
@@ -217,14 +242,6 @@ CKEDITOR.replace( 'new_mix', {
                                     </div>
                                 </div>
                                 <!-- /Modal -->
-                                <form action="{{ url('/doc/graduate/'.$g->id.'/edit') }}" method="GET">
-                                    <button type="submit" class="btn btn-success btn-lg" id="edit-graduate-{{ $g->id }}">編輯</button>
-                                </form>
-                                <form action="{{ url('/doc/graduate/'.$g->id) }}" method="POST">
-                                    {!! csrf_field() !!}
-                                    {!! method_field('DELETE') !!}
-                                    <button type="submit" class="btn btn-danger btn-lg" id="delete-document-{{ $g->id }}">刪除</button>
-                                </form>
                             </div>
                         @endforeach
                         </div>
@@ -242,13 +259,13 @@ CKEDITOR.replace( 'new_mix', {
                 </ul>
                 <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal-new-graduate">新增</button>
                 <!-- Modal -->
-                <div id="modal-new-graduate" class="modal fade" role="dialog">
+                <div id="modal-new-graduate" class="modal fade text-left" role="dialog">
                     <div class="modal-dialog modal-lg">
                         <!-- Modal content-->
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">研究所 - 新增內容</h4>
+                                <h1 class="modal-title">研究所 - 新增內容</h1>
                             </div>
                             <div class="modal-body">
                                 <form action="{{ url('/doc/graduate') }}" method="POST">
@@ -259,11 +276,11 @@ CKEDITOR.replace( 'new_mix', {
                                     <p><textarea name="content" id="new_gra" required></textarea></p>
                                     <p>隸屬於哪個主項目</p>
                                     <p><input type="number" name="position_of_main" min="1" max="3" step="1" value="1" required></p>
-                                    <p><button type="submit" class="btn btn-primary">新增</button></p>
+                                    <p>
+                                        <button type="submit" class="btn btn-primary">新增</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">關閉</button>
+                                    </p>
                                 </form> 
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">關閉</button>
                             </div>
                         </div>
                     </div>
@@ -289,7 +306,7 @@ CKEDITOR.replace( 'new_mix', {
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">共同 - 新增內容</h4>
+                                <h1 class="modal-title">共同 - 新增內容</h1>
                             </div>
                             <div class="modal-body">
                                 <form action="{{ url('/doc/mix') }}" method="POST">
@@ -336,7 +353,7 @@ CKEDITOR.replace( 'new_mix', {
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">{{ $m->title }}</h4>
+                                <h1 class="modal-title">{{ $m->title }}</h1>
                             </div>
                             <div class="modal-body">
                                 <div class="row">
