209ad1d467a2ccc9fc0a55af8af212c931ab7536
Hsu <edittye379@gmail.com>
2016-08-01 00:52:20 +0800

    bug

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,7 @@ $("document").ready(function () {
     // scroll 監聽
     var window_height = 0;//$(window).height() - 50;
     var currentScroll = $(this).scrollTop();
-    if (currentScroll < window_height ) {
+    if (currentScroll <= window_height ) {
         $('.navbar.navbar-ncufresh').css('background-color','transparent');
     }
     $(window).scroll(function () {
