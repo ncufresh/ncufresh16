2ff67cfc0ac9c2fe6e2a100f2aea2d04f6973695
twinkle8s <twinklecindy8s@gmail.com>
2016-08-05 22:37:30 +0800

    program team

--- resources/views/us/plan.blade.php
+++ resources/views/us/plan.blade.php
@@ -9,14 +9,16 @@
 				</div>
 				<div class="modal-body row">
 					<div class="col-xs-12 col-sm-5 center-block">
-						<img src="{{ asset('img/us/plan1.jpg') }}" class="introduce">
+						<img src="{{ asset('img/us/plan1.jpg') }}" class="img-rounded introduce">
 					</div>
-					<div class="col-xs-12 col-sm-7 introduce">
+					<div class="col-xs-12 col-sm-7">
+						<div class="introduce">
 						姓名：楊雅雯<br>
 						系級：資管2B<br>
 						組別：企劃組<br>
 						負責：小遊戲、個人專區<br>
 						想說的一句話：嗨嗨嗨
+						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -36,14 +38,16 @@
 				</div>
 				<div class="modal-body row">
 					<div class="col-xs-12 col-sm-5 center-block">
-						<img src="{{ asset('img/us/plan2.jpg') }}" class="introduce">
+						<img src="{{ asset('img/us/plan2.jpg') }}" class="img-rounded introduce">
 					</div>
-					<div class="col-xs-12 col-sm-7 introduce">
+					<div class="col-xs-12 col-sm-7">
+						<div class="introduce">
 						姓名：劉世文<br>
 						系級：財金二<br>
 						組別：企劃組<br>
 						負責：中大生活、影音專區<br>
 						想說的一句話：啾啾
+						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -63,14 +67,16 @@
 				</div>
 				<div class="modal-body row">
 					<div class="col-xs-12 col-sm-5 center-block">
-						<img src="{{ asset('img/us/plan3.jpg') }}" class="introduce">
+						<img src="{{ asset('img/us/plan3.jpg') }}" class="img-rounded introduce">
 					</div>
-					<div class="col-xs-12 col-sm-7 introduce">
+					<div class="col-xs-12 col-sm-7">
+						<div class="introduce">
 						姓名：林煒婷<br>
 						系級：資工3A<br>
 						組別：企劃組(組長)<br>
 						負責：首頁、新生必讀、打雜、陪執行長吃飯<br>
 						想說的一句話：...
+						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -90,14 +96,16 @@
 				</div>
 				<div class="modal-body row">
 					<div class="col-xs-12 col-sm-5 center-block">
-						<img src="{{ asset('img/us/plan4.jpg') }}" class="introduce">
+						<img src="{{ asset('img/us/plan4.jpg') }}" class="img-rounded introduce">
 					</div>
-					<div class="col-xs-12 col-sm-7 introduce">
+					<div class="col-xs-12 col-sm-7">
+						<div class="introduce">
 						姓名：楊祺笙<br>
 						系級：資工三A<br>
 						組別：企劃組(組長)<br>
 						負責：打雜<br>
 						想說的一句話：輕輕鬆鬆
+						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -117,19 +125,47 @@
 				</div>
 				<div class="modal-body row">
 					<div class="col-xs-12 col-sm-5 center-block">
-						<img src="{{ asset('img/us/plan5.jpg') }}" class="introduce">
+						<img src="{{ asset('img/us/plan5.jpg') }}" class="img-rounded introduce">
 					</div>
-					<div class="col-xs-12 col-sm-7 introduce">
+					<div class="col-xs-12 col-sm-7">
+						<div class="introduce">
 						姓名：羅浩恩<br>
 						系級：資訊工程二年級<br>
 						組別：企劃組<br>
 						負責：校園導覽、關於我們<br>
 						想說的一句話：FB缺讚友+++
+						</div>
 					</div>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal text-center" id="plan6">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">x</button>
+			</div>
+			<div class="modal-body row">
+				<div class="col-xs-12 col-sm-5 center-block">
+					<img src="{{ asset('img/us/plan6.jpg') }}" class="img-rounded introduce">
+				</div>
+				<div class="col-xs-12 col-sm-7">
+					<div class="introduce">
+					姓名：賴子安<br>
+					系級：企管三<br>
+					組別：企劃組<br>
+					負責：QA<br>
+					想說的一句話：謝謝大家:)
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 			</div>
 		</div>
 	</div>
--- resources/views/us/team.blade.php
+++ resources/views/us/team.blade.php
@@ -10,19 +10,23 @@
 		@elseif($team=="plan")
 			<a data-toggle="modal" data-target="#plan1"><img src="{{ asset('img/us/blank.png') }}" id="img-plan1"></a>
 			<a data-toggle="modal" data-target="#plan2"><img src="{{ asset('img/us/blank.png') }}" id="img-plan2"></a>
-			<!-- !!!!!!!!!!!!!!!!!!-->
+			<a data-toggle="modal" data-target="#plan3"><img src="{{ asset('img/us/blank.png') }}" id="img-plan3"></a>
+			<a data-toggle="modal" data-target="#plan4"><img src="{{ asset('img/us/blank.png') }}" id="img-plan4"></a>
+			<a data-toggle="modal" data-target="#plan5"><img src="{{ asset('img/us/blank.png') }}" id="img-plan5"></a>
+			<a data-toggle="modal" data-target="#plan6"><img src="{{ asset('img/us/plan6.jpg') }}" class="img-circle" id="img-plan6"></a>
+			<a data-toggle="modal" data-target="#plan6" class="about-size" id="name-plan6">賴<br>子<br>安</a>
 		@elseif($team=="program")
-			<h3>程式設計組</h3>
+			
 		@elseif($team=="art")
-			<h3>網頁美工組</h3>
+			
 		@else($team=="video")
-			<h3>媒體影音組</h3>
+			
 		@endif
 		</div>
 		
 		<div class="caption about-size row">
 		@if($team=="execute")
-			成員：
+			
 		@elseif($team=="plan")
 			<style>
 				a#plan {
@@ -30,8 +34,31 @@
 				}
 			</style>
 			@include('us.plan')
+			<div class="col-xs-12">
+				<div class="team-introduce center-block">
+					<p>
+					<br>
+					&emsp;&emsp;企劃組決定了知訊網的首頁風格，以及各種項目的網頁版面、內容。將自己的想法告訴美工組及程設組，由美工使網頁更加生動活潑、由程設使網頁能呈現於新生面前，打造出理想中的知訊網。
+					</p>
+					<a href="." class="btn btn-default pull-right">Return</a>
+				</div>
+			</div>
 		@elseif($team=="program")
-			<h3>程式設計組</h3>
+			<style>
+				a#program {
+					color: #6280a1;
+				}
+			</style>
+			@include('us.program')
+			<div class="col-xs-12">
+				<div class="team-introduce center-block">
+					<p>
+					<br>
+					&emsp;&emsp;程設組負責架構出網站，使網站有著各種功能，並且運用程式碼完成企劃組所設計出的風格，是將整個知訊網推動的幕後功臣，將帶給新生最好的知訊網體驗。
+					</p>
+					<a href="." class="btn btn-default pull-right">Return</a>
+				</div>
+			</div>
 		@elseif($team=="art")
 			<h3>網頁美工組</h3>
 		@else($team=="video")
