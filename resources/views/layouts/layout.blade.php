6b964314a14671f4eddf8751776bb571958b278f
Hsu <edittye379@gmail.com>
2016-08-08 05:09:13 +0800

    ogp photo

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,10 +9,11 @@
     {{-- fb --}}
     <meta property="og:url" content="http://lovenery.me" />
     <meta property="og:title" content="中央大學 | 2016 新生知訊網" />
-	<meta property="og:site_name" content="新生知訊網"/>
-	<meta property="og:image" content="{{asset('img/layout/fbogp.jpg')}}"/>
-	<meta property="og:description" content="為歡迎大學新鮮人，新生知訊網，主打各項貼心服務，從飲食、住宿、交通、教育、藝文等主題出發的生活資訊，與大考放榜同步上線"/>
-	<meta property="og:type" content="website">
+    <meta property="og:image" content="{{asset('img/layout/fbogp.jpg')}}"/>
+    <meta property="og:description" content="為歡迎大學新鮮人，新生知訊網，主打各項貼心服務，從飲食、住宿、交通、教育、藝文等主題出發的生活資訊，與大考放榜同步上線"/>
+    <meta property="og:type" content="website">
+    {{-- title --}}
+    <title>@yield('title', '我就說不是改我了!!')</title>
     {{-- 引入Styles --}}
     <link rel="stylesheet" href="{{ asset('include/bootstrap/css/bootstrap.min.css') }}" media="screen">
     <link rel="stylesheet" href="{{ asset('include/font-awesome/css/font-awesome.min.css') }}" media="screen">
@@ -30,7 +31,6 @@
     {{-- 個人Styles --}}
     <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
     @yield('css')
-    <title>@yield('title', '我就說不是改我了!!')</title>
 </head>
 <body id="app-layout">
 
